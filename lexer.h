1ebebcc42933569d0f028f9196fea1cbebce5edd
nikit34 <permikov134@mail.ru>
2021-03-30 06:01:57 +0300

    reight

--- lexer.h
+++ lexer.h
@@ -6,21 +6,43 @@ class tLexer:public tBaseLexer{
 public:
   // конструктор
   tLexer() : tBaseLexer() {
+
+    // lab4
+  addstr(Adec, 0, "+-", 2);
+  addrange(Adec, 0, '0', '9', 1);
+  addrange(Adec, 1, '0', '9', 1);
+  addrange(Adec, 2, '0', '9', 1);
+  addstr(Adec, 1, ".", 3);
+  addrange(Adec, 3, '0', '9', 4);
+  addrange(Adec, 4, '0', '9', 4);
+  addstr(Adec, 4, "eE", 5);
+  addstr(Adec, 5, "+-", 7);
+  addrange(Adec, 5, '0', '9', 6);
+  addrange(Adec, 7, '0', '9', 6);
+  Adec.final(6);
+
+
   //создать автоматы: 0-9 a-z A-Z - ? !
 
   // идентификатор !--    0-9*? -*?
   addrange(Aid, 0, 'a', 'z', 1);
   addrange(Aid, 0, 'A', 'Z', 1);
   addstr(Aid, 0, "?", 1);
-  addrange(Aid, 1, 'a', 'z', 1);
-  addrange(Aid, 1, 'A', 'Z', 1);
-  addrange(Aid, 1, '0', '9', 1);
-  addstr(Aid, 1, "?", 1);
-  addstr(Aid, 1, "!", 1);
   addrange(Aid, 1, 'a', 'z', 2);
   addrange(Aid, 1, 'A', 'Z', 2);
   addrange(Aid, 1, '0', '9', 2);
+  addstr(Aid, 1, "?", 2);
   addstr(Aid, 1, "!", 2);
+  addrange(Aid, 2, 'a', 'z', 2);
+  addrange(Aid, 2, 'A', 'Z', 2);
+  addrange(Aid, 2, '0', '9', 2);
+  addstr(Aid, 2, "?", 3);
+  addstr(Aid, 2, "!", 2);
+  addrange(Aid, 3, 'a', 'z', 2);
+  addrange(Aid, 3, 'A', 'Z', 2);
+  addrange(Aid, 3, '0', '9', 2);
+  addstr(Aid, 3, "!", 2);
+  addstr(Aid, 3, "?", 3);
   Aid.final(2);
 
 
